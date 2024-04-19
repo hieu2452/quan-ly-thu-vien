18e90ee403f5f3eb3f7498dc946618d6ddc85c08
hieu2452 <hieu245202@gmail.com>
2024-04-19 15:49:16 +0700

    set up

---
+++ lb-management/src/main/java/com/hieu/lbmanagement/LbManagementApplication.java
@@ -0,0 +1,13 @@
+package com.hieu.lbmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbManagementApplication.class, args);
+	}
+
+}
---
+++ lb-management/src/test/java/com/hieu/lbmanagement/LbManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hieu.lbmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
